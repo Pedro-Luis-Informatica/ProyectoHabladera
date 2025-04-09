621923b3dfe8f895cc8b831af5395f2935d52f65
Pedro-Luis-Informatica <valenzuelapedroluis338@gmail.com>
2025-04-09 11:33:04 -0400

    Cofiguracion de las tarjetas

---
+++ src/js/generadorCards.js
@@ -0,0 +1,7 @@
+// funcion para generar nuestras tarjetas de podcasts
+// import { podcasts } from "./podcasts";
+
+const generadorCards = (podcast) => {
+  const cardElement = document.createElement("li");
+  cardElement.classList.add("tarjeta");
+};
